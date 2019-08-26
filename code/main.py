9599587c584a08b023e06040c0564982eb6736f4
chi <w121210@hotmail.com>
2019-08-26 20:42:18 +0700

    .

--- code/main.py
+++ code/main.py
@@ -7,11 +7,13 @@ import os
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.1)
 current_dir = os.path.dirname(__file__)
-weights_dir = os.path.join(current_dir, '..', 'weights')
+weights_dir = os.path.join(current_dir, "..", "weights")
+
+
 def train(epoch):
-    print('#'*15)
-    print('Epoch {}, Latent Size {}'.format(epoch, model.latent_size))
-    print('#'*15)
+    print("#" * 15)
+    print("Epoch {}, Latent Size {}".format(epoch, model.latent_size))
+    print("#" * 15)
     model.train()
     for index, (x, _) in enumerate(loader):
         x = x.mean(dim=1, keepdim=True).to(device)
@@ -21,13 +23,14 @@ def train(epoch):
         loss.backward()
         optimizer.step()
         if index % 100 == 0:
-            print('Loss at iteration {0}: {1:.4f}'.format(index, loss.item()))
+            print("Loss at iteration {0}: {1:.4f}".format(index, loss.item()))
     if epoch == 4:
-        filename = 'epoch{}_ls{}.pkl'.format(epoch, model.latent_size)
+        filename = "epoch{}_ls{}.pkl".format(epoch, model.latent_size)
         torch.save(model.state_dict(), os.path.join(weights_dir, filename))
     if epoch < 4:
         scheduler.step()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     for epoch in range(5):
         train(epoch)
